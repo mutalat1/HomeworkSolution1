2efd2c702a3299ed7bf44e9926b44c3d32ed9041
mutalat1 <alharm870@gmail.com>
2025-02-21 03:05:24 +0200

    First commit

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double num1, num2;
+
+    //inputs
+	cin >> num1 >> num2;
+
+	// operations
+	cout << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
+	cout << num1 << " - " << num2 << " = " << num1 - num2 << "\n";
+	cout << num1 << " / " << num2 << " = " << num1 / num2 << "\n";
+	cout << num1 << " * " << num2 << " = " << num1 * num2 << "\n";
+}
